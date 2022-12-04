09a5b10777016b13a46d016f201f39f59d73c86a
Robbie Luijben <luijben@astron.nl>
2022-12-04 11:01:47 +0100

    Day 4 in C# + fix gitignore for each project

---
+++ csharp/AdventOfCode2022/04.02/Program.cs
@@ -0,0 +1,12 @@
+﻿var count =
+        (await File.ReadAllLinesAsync("input.txt"))
+        .Select(line =>
+            line.Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Select(pair => pair.Split("-", StringSplitOptions.RemoveEmptyEntries))
+                .Select(pair => new { Min = int.Parse(pair[0]), Max = int.Parse(pair[1]) }).ToArray())
+        .Count(pair =>
+            (pair[0].Min >= pair[1].Min && pair[0].Min <= pair[1].Max) ||
+            (pair[1].Min >= pair[0].Min && pair[1].Min <= pair[0].Max)
+        );
+
+Console.WriteLine(count);
